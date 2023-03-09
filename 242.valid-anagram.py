b0bdf34dd7ed5ce94e15e22d873bc9487b9432dc
Anthony Mai <Anthony.Mai@hotmail.com>
2023-03-09 13:24:41 +1100

    Improved initial solution, added additional solution

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -7,28 +7,36 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        charsInS = {}
 
-        for char in s:
-            if char in charsInS:
-                charsInS[char] = charsInS[char] + 1
-            else:
-                charsInS[char] = 1
+        # Solution 1 (Hash Map)
 
-        for char in t:
-            if not char in charsInS:
-                return False
-            else:
-                if charsInS[char] > 0:
-                    charsInS[char] = charsInS[char] - 1
-                else:
-                    return False
-        
-        for key, value in charsInS.items():
-            if value > 0:
-                return False
+        # if len(s) != len(t):
+        #     return False
 
-        return True
+        # charsInS, charsInT = {}, {}
+
+        # # Loop through each char in s
+        # # Increment the letter at s[i] by 1 (default to 0+1 if not in map)
+        # # Increment the letter at t[i] by 1 (default to 0+1 if not in map)
+        # for i in range(len(s)):
+        #     charsInS[s[i]] = charsInS.get(s[i], 0) + 1
+        #     charsInT[t[i]] = charsInT.get(t[i], 0) + 1
+
+        # # Loop through each char in s map
+        # # Check that the value of letter matches the value in t map
+        # for c in charsInS:
+        #     if charsInS[c] != charsInT.get(c, 0):
+        #         return False
+
+        # return True
+    
+        # Solution 2 (Sort)
+
+        # Sort teach string, if they're an anagram then the
+        # each resulting string should be equal
+        #
+        # Complexity depends on which sorting algorithm is used
+        return sorted(s) == sorted(t)
 
 
 # @lc code=end
