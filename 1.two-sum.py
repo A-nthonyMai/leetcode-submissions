683e2e46c65a58fd828ffc26fa43505e88ee3acb
Anthony Mai <Anthony.Mai@hotmail.com>
2023-03-09 15:30:30 +1100

    Added one pass solution for two sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,9 +7,20 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Solution 1 - Brute Force
+
+        # for index, num in enumerate(nums):
+        #     for index2, num2 in enumerate(nums[index+1:], index+1):
+        #         if num + num2 == target:
+        #             return [index, index2]
+
+        # Solution 2 - One Pass
+        mapOfPrevNums = {}
+
         for index, num in enumerate(nums):
-            for index2, num2 in enumerate(nums[index+1:], index+1):
-                if num + num2 == target:
-                    return [index, index2]
+            otherNum = target - num
+            if otherNum in mapOfPrevNums:
+                return [mapOfPrevNums[otherNum], index]
+            mapOfPrevNums[num] = index 
 # @lc code=end
 
